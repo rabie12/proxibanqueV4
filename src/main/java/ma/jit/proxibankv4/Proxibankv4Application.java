df9cfba824af06c1f69534998089b29f67431ad9
HABACHI <rabiie.habachi@gmail.com>
2019-10-14 23:54:31 +0100

    Création de projet

---
+++ src/main/java/ma/jit/proxibankv4/Proxibankv4Application.java
@@ -0,0 +1,13 @@
+package ma.jit.proxibankv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proxibankv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proxibankv4Application.class, args);
+	}
+
+}
